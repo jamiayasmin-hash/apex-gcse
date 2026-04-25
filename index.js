91b167c762ea724c726143ce4c5869a06eb2a6c1
jamiayasmin-hash <jamiayasmin@gmail.com>
2026-04-25 16:27:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+  <rect width="512" height="512" rx="80" fill="#0a0d14"/>
+  <text x="256" y="340" font-size="300" text-anchor="middle" font-family="system-ui">🏔️</text>
+  <text x="256" y="460" font-size="72" text-anchor="middle" font-family="system-ui, sans-serif" font-weight="800" fill="#f59e0b">APEX</text>
+</svg>
